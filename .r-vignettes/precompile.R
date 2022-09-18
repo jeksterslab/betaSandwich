c47ac028a219e4e9832ca77ceb6c7d1270bf7bb4
jeksterslab <learn.jeksterslab@gmail.com>
2022-09-18 20:07:09 +0000

    Automated build.

---
+++ .r-vignettes/precompile.R
@@ -0,0 +1,32 @@
+root <- rprojroot::is_rstudio_project
+vignettes_folder <- root$find_file(
+  "vignettes"
+)
+vignettes_source <- root$find_file(
+  ".r-vignettes"
+)
+orig <- list.files(
+  path = vignettes_source,
+  pattern = "\\.orig$",
+  full.names = FALSE,
+  all.files = TRUE,
+  recursive = FALSE
+)
+if (length(orig) > 0) {
+  file.copy(
+    from = file.path(vignettes_source, orig),
+    to = vignettes_folder,
+    overwrite = TRUE
+  )
+  target <- gsub(
+    pattern = "\\.orig$",
+    replacement = "",
+    x = orig
+  )
+  for (i in seq_along(orig)) {
+    knitr::knit(
+      file.path(vignettes_folder, orig[i]),
+      file.path(vignettes_folder, target[i])
+    )
+  }
+}
