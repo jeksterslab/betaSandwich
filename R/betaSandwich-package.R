84ccbb1b1f847c1524b867da14d8d52309202721
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2023-08-19 17:25:58 +0000

    Automated build.

--- R/betaSandwich-package.R
+++
@@ -1,7 +0,0 @@
-#' @aliases betaSandwich-package
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
-NULL
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(betaSandwich)
-
-test_check("betaSandwich")
