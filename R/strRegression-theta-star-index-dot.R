0d2e00c64619f343f6be69c1d241a8b80d26defb
jeksterslab <jeksterslab@users.noreply.github.com>
2023-10-03 01:17:01 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/betaSandwich.pdf
    A	.setup/build/betaSandwich_1.0.5.9000.tar.gz
    A	.setup/latex/bib/quarto.bib
    A	.setup/lint/.lintr
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE
    A	LICENSE.md
    A	NAMESPACE
    A	R/dataSets-nas1982.R
    A	R/gammaADF-gamma-adf-consistent-dot.R
    A	R/gammaADF-gamma-adf-unbiased-dot.R
    A	R/gammaN-gamma-mvn-dot.R
    A	R/linearAlgebra-d-mat-dot.R
    A	R/linearAlgebra-d-of-mat-dot.R
    A	R/linearAlgebra-pinv-of-d-mat-dot.R
    A	R/linearAlgebra-vec-dot.R
    A	R/linearAlgebra-vech-dot.R
    A	R/linearAlgebra-vech-names-dot.R
    A	R/linearAlgebra-vechs-dot.R
    A	R/linearAlgebra-vechs-names-dot.R
    A	R/processLM-dif-dot.R
    A	R/processLM-process-lm-dot.R
    A	R/rhoMatrix-rho-of-sigma-dot.R
    A	R/strRegression-beta-star-of-rho-dot.R
    A	R/strRegression-jacobian-vech-sigma-wrt-theta-dot.R
    A	R/strRegression-jacobian-vech-sigma-wrt-theta-std-dot.R
    A	R/strRegression-moments-index-dot.R
    A	R/strRegression-p-cor-sq-dot.R
    A	R/strRegression-r-sq-bar-dot.R
    A	R/strRegression-r-sq-of-sigma-dot.R
    A	R/strRegression-s-p-cor-dot.R
    A	R/strRegression-theta-index-dot.R
    A	R/strRegression-theta-star-index-dot.R
    A	R/wald-ci-wald-dot.R
    A	R/wald-probs-of-alpha-dot.R
    A	README.md
    M	data/nas1982.rda
    A	man/BetaADF.Rd
    A	man/BetaHC.Rd
    A	man/BetaN.Rd
    A	man/DiffBetaSandwich.Rd
    A	man/RSqBetaSandwich.Rd
    A	man/betaSandwich-package.Rd
    A	man/coef.betasandwich.Rd
    A	man/coef.diffbetasandwich.Rd
    A	man/coef.rsqbetasandwich.Rd
    A	man/confint.betasandwich.Rd
    A	man/confint.diffbetasandwich.Rd
    A	man/confint.rsqbetasandwich.Rd
    A	man/nas1982.Rd
    A	man/print.betasandwich.Rd
    A	man/print.diffbetasandwich.Rd
    A	man/print.rsqbetasandwich.Rd
    A	man/summary.betasandwich.Rd
    A	man/summary.diffbetasandwich.Rd
    A	man/summary.rsqbetasandwich.Rd
    A	man/vcov.betasandwich.Rd
    A	man/vcov.diffbetasandwich.Rd
    A	man/vcov.rsqbetasandwich.Rd
    M	project.Rproj
    A	vignettes/vignettes.bib

---
+++ R/strRegression-theta-star-index-dot.R
@@ -0,0 +1,59 @@
+#' Create Index for the Standardized Parameter Vector
+#'
+#' @param p Positive integer.
+#'   `p` regressors.
+#'
+#' @return Returns a list of indices.
+#' @family Standardized Parameters Functions
+#' @keywords strRegression parametersstd internal
+#' @noRd
+.ThetaStarIndex <- function(p) {
+  if (p == 1) {
+    return(
+      list(
+        betastar = paste0(
+          "betastar",
+          seq_len(p)
+        ),
+        sigmay = "sigmay",
+        sigmax = paste0(
+          "sigmax",
+          seq_len(p)
+        ),
+        sigmastarsq = "sigmastarsq",
+        muy = "muy",
+        mux = paste0(
+          "mux",
+          seq_len(p)
+        )
+      )
+    )
+  } else {
+    return(
+      list(
+        betastar = paste0(
+          "betastar",
+          seq_len(p)
+        ),
+        sigmay = "sigmay",
+        sigmax = paste0(
+          "sigmax",
+          seq_len(p)
+        ),
+        vechsrhocapx = paste0(
+          "rho",
+          .VechsNames(
+            x = paste0("x", seq_len(p)),
+            sep = ""
+          )
+        ),
+        sigmastarsq = "sigmastarsq",
+        muy = "muy",
+        mux = paste0(
+          "mux",
+          seq_len(p)
+        )
+      )
+    )
+  }
+}
